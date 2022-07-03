1975ba08deaebb76343f77a17469ec1eb644509c
Javier Rodriguez <javicerodriguez@gmail.com>
2022-07-03 01:53:42 -0300

    ✨ add: filter users by name & status

--- src/pages/usuarios/index.jsx
+++ src/pages/usuarios/index.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
+import { Input, HStack, Select } from '@chakra-ui/react';
 import UserCard from "@components/common/Cards/User";
 import MainLayout from "@components/layout/MainLayout";
 
 const Users = ({ users }) => {
   const [usersList, setUsersList] = useState(users);
+  const [filter, setFilter] = useState({
+    name: '',
+    status: null,
+  });
 
   const changeStatus = (userUid, status) => {
     const newUsersList = usersList.map(user => {
@@ -15,12 +20,48 @@ const Users = ({ users }) => {
     setUsersList(newUsersList);
   }
 
+  const filterUsers = (key, value) => {
+    const newFilter = { ...filter };
+    newFilter[key] = value;
+    setFilter(newFilter);
+  }
+
   useEffect(() => {
     setUsersList(users);
   }, []);
 
+  useEffect(() => {
+    // Filter users by first_name + last_name and status
+    const newUsersList = users.filter(user => {
+      let result = true;
+      if (filter.name) {
+        result = user.first_name.toLowerCase().includes(filter.name.toLowerCase()) || user.last_name.toLowerCase().includes(filter.name.toLowerCase());
+      }
+      if (filter.status) {
+        result = result && user.subscription.status === filter.status;
+      }
+      return result;
+    });
+    setUsersList(newUsersList);
+  }, [filter])
+
   return (
     <MainLayout>
+      <HStack mx="8px" my="12px">
+        <Input
+          variant='outline'
+          placeholder='Buscar por nombre'
+          onChange={(e) => filterUsers('name', e.target.value)}
+        />
+        <Select
+          placeholder='Filtrar por estado'
+          onChange={(e) => filterUsers('status', e.target.value)}>
+          <option value='Active'>Activo</option>
+          <option value='Pending'>Pediente</option>
+          <option value='Idle'>Inactivo</option>
+          <option value='Blocked'>Bloqueado</option>
+        </Select>
+      </HStack>
       {
         usersList && usersList.map(user => <UserCard key={user.uid} user={user} changeStatus={changeStatus} />)
       }
